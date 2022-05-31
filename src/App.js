f5d7756abbd4bfca479be6d90f21b9fce5e26abe
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-05-31 22:37:27 +0400

    docker test frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = React.useState({})
+
+  React.useEffect(() => {
+    fetch('http://localhost:8000/').then(res => res.json()).then(data => setMessage(data))
+  },[])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{JSON.stringify(message)}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
